9ac897ef804a2d4cf2f4abdd1f9e761621edf237
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-05-01 09:09:12 +0300

    FriendListItem

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+
+
+
+
+
+/*<ul class="friend-list">
+
+</ul>
+
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
+*/
